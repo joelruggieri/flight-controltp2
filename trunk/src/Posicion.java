0e0e9628511a8b31a498871f64cc192f71373638
joelruggieri@gmail.com <joelruggieri@gmail.com@c7857a9e-41aa-a20c-209e-0a810bdc360d>
2012-11-03 23:51:19 +0000

    los setters de la posicion y su constructor

---
+++ trunk/src/Posicion.java
@@ -0,0 +1,27 @@
+
+public class Posicion {
+	
+	private int x;
+	private int y;
+	
+	
+	public Posicion(int coordenadaX, int coordenadaY){
+		
+		
+		this.x = coordenadaX;
+		this.y = coordenadaY;
+	}
+	
+	public int getPosicionX (){
+		
+		return this.x;
+	}
+
+	
+	public int getPosicionY (){
+		
+		return this.y;
+	}
+	
+
+}
