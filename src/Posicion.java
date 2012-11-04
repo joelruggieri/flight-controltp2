15e2b9316150f00270b0c6e5fda6ed75ceaedf3f
gvallerino <gvallerino@c7857a9e-41aa-a20c-209e-0a810bdc360d>
2012-11-04 20:54:56 +0000

    Agregué los métodos sumar y restar de la posición. Además hice los comentarios.

--- src/Posicion.java
+++ src/Posicion.java
@@ -1,3 +1,4 @@
+
 public class Posicion {
 	
 	private int x;
@@ -5,28 +6,61 @@ public class Posicion {
 	
 	
 	public Posicion(int coordenadaX, int coordenadaY){
-		
+		/* Constructor de la clase Posicion */
+		/* pre: Deben ingresarse dos enteros */
+		/* post: La posicion fue creada */
 		
 		this.x = coordenadaX;
 		this.y = coordenadaY;
 	}
 	
 	public int getPosicionX (){
-		
+		/* Devuelve la coordenada x */
 		return this.x;
 	}
 
-	
 	public int getPosicionY (){
-		
+		/* Devuelve la coordenada y */
 		return this.y;
 	}
-  
-  public boolean equals (Posicion otraPosicion){
-		/* Devuelve si otraPosicion es igual */
-      
+	
+	public boolean equals (Posicion otraPosicion){
+		/* Devuelve si la Posicion es igual a la posicion pasada por parametro */
+		/* pre: Debe ingresarse una posicion */
+		
 		return (this.x == otraPosicion.getPosicionX()) && (this.y == otraPosicion.getPosicionY());
 	}
 	
+	public Posicion sumar (Posicion otraPosicion){
+		/* Suma la posicion con la posicion pasada por parametro */
+		/* pre: Debe ingresarse una posicion */
+		/* post: Devuelve la posicion suma */
+		
+		int x,y;
+		Posicion suma;
+		
+		x = this.x + otraPosicion.getPosicionX();
+		y = this.y + otraPosicion.getPosicionY();
+		
+		suma = new Posicion(x,y);
+		
+		return suma;
+	}
+	
+	public Posicion restar (Posicion otraPosicion){
+		/* Resta la posicion con la posicion pasada por parametro */
+		/* pre: Debe ingresarse una posicion */
+		/* post: Devuelve la posicion resta */
+		
+		int x,y;
+		Posicion resta;
+		
+		x = this.x - otraPosicion.getPosicionX();
+		y = this.y - otraPosicion.getPosicionY();
+		
+		resta = new Posicion(x,y);
+		
+		return resta;
+	}
 
 }
