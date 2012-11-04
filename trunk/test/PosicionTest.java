96627309609b8d0d6a6ca4d84157acd4898d6c33
gvallerino@gmail.com <gvallerino@gmail.com@c7857a9e-41aa-a20c-209e-0a810bdc360d>
2012-11-04 04:38:48 +0000

    Agregué dos test mas, que prueban el método equlas.

--- trunk/test/PosicionTest.java
+++ trunk/test/PosicionTest.java
@@ -12,5 +12,41 @@ public class PosicionTest extends TestCase {
 		assertEquals ( coordenadaX, unaPosicion.getPosicionX());
 		assertEquals (coordenadaY, unaPosicion.getPosicionY());
 	}
+   
+  public void testDeDosPosicionesIgualesEqualsDeberiaDarVerdadero () {		
+		
+		//arrange
+		int x = 1;
+		int y = 5;
+		boolean sonIguales;
+		
+		Posicion unaPosicion = new Posicion (x,y);
+		Posicion otraPosicion = new Posicion (x,y);
+		
+		//act
+		sonIguales = unaPosicion.equals(otraPosicion);
+		
+		//assert
+		assertTrue (sonIguales);
+		
+	}
+   
+  public void testDeDosPosicionesDistintasEqualsDeberiaDarFalso () {		
+		
+		//arrange
+		int x = 1;
+		int y = 5;
+		boolean sonIguales;
+		
+		Posicion unaPosicion = new Posicion (x,y);
+		Posicion otraPosicion = new Posicion (x,1);
+		
+		//act
+		sonIguales = unaPosicion.equals(otraPosicion);
+		
+		//assert
+		assertFalse (sonIguales);
+		
+	}
 
 }
