f8646d7a53b9ef1bbea01a83431a231ead7b4288
KeyNaka <KeyNaka@c7857a9e-41aa-a20c-209e-0a810bdc360d>
2012-11-06 19:47:29 +0000

    Se creo la clase helicoptero y se cambiaron los nombres de la clase simple a avionSimple y avion a objetoVolador.

--- src/ObjetoVolador.java
+++ src/ObjetoVolador.java
@@ -1,14 +1,15 @@
 import java.util.ArrayList;
 
-public abstract class Avion {
+public abstract class ObjetoVolador {
 	private int velocidad; //buscar para hacerlo atributo de clase
-	private Posicion posicionActual;
-	private boolean aterrizado; 
-	private Posicion direccion;
-	private Trayectoria trayectoria;
+	protected Posicion posicionActual;
+	protected boolean aterrizado; 
+	protected Posicion direccion;
+	protected Trayectoria trayectoria;
 	
 	
-	public Avion(int velocidad){
+	public ObjetoVolador(int velocidad){
+		/* Constructor del Objeto volador */
 		
 		int valorDeSalidaX = 0;
 		int valorDeSalidaY = 0;
@@ -22,33 +23,41 @@ public abstract class Avion {
 	}
 	
 	public void crearTrayectoria(ArrayList<Posicion> unaTrayectoriaNueva){
+		/* Crea una nueva trayectoria para el objeto volador */
 		
 		this.trayectoria = new Trayectoria(unaTrayectoriaNueva);
 		
 	}
 	
 	public void aterrizar(){
+		/* Aterriza el objeto volador en una Pista */
+		/* pre: debe de aterrizar */
+		/* post: cambia el estado del objeto volador a aterrizado */
 		
 		this.aterrizado = true;
 		
 	}
 
 	public void setPosicion(Posicion unaPosicion){
+		/* Establece la posicion actual del objeto volador */
 		
 		this.posicionActual = unaPosicion;
 	}
 	
 	public Posicion getPosicion(){
+		/* Devuelve la posicion actual del objeto volador */
 		
 		return this.posicionActual;
 	}
 	
 	public void setDireccion(Posicion posicion){
+		/* Establece la direccion actual del objeto volador */
 		
 		this.direccion = posicion;
 	}
 	
 	public Posicion getDireccion(){
+		/* Devuelve la direccion actual del objeto volador */
 		
 		return this.direccion;
 		
@@ -56,18 +65,29 @@ public abstract class Avion {
 	
 	
 	public void moverse(){
+		/* Mueve el objeto volador siguiendo la trayectoria o en el sentido de la direccion si no hay trayectoria definida */
+		/* post: cambia el valor de la posicion actual, buscando el movimiento mas optimo posible */
 		
+		if (this.trayectoria.getPrimerPosicion() == null){
+			
+			this.posicionActual = this.posicionActual.sumar(this.direccion);
+			
+		}else{
+			
+			this.trayectoria.calcularProximaPosicion(posicionActual);
+			
+		}
 		
 	}
 	
-
-	
-	
+/*	
 	public void invertirDireccion(){
 		int nuevaDireccionEnX = this.getDireccion().getPosicionX() * (-1);
 		int nuevaDireccionEnY = this.getDireccion().getPosicionY() * (-1);
 		
 		this.direccion = new Posicion(nuevaDireccionEnX, nuevaDireccionEnY);
 	}
+*/
+	
 }
 	
--- src/Trayectoria.java
+++ src/Trayectoria.java
@@ -5,17 +5,28 @@ public class Trayectoria {
 	private ArrayList<Posicion> listaDePosiciones;
 	
 	public Trayectoria (ArrayList<Posicion> unaListaDePosiciones){
+		/* Constructor de una trayectoria */
+		/* pre: una lista de posiciones con posiciones, o vacia */
+		/* post: establece la trayectoria deseada */
 		
 		this.listaDePosiciones = unaListaDePosiciones;
 		
 	}	
 	
-	public Posicion calcularProximaPosicion(){
-		
+	public Posicion calcularProximaPosicion(Posicion posicionActual){
+		/* Calcula la proxima posicion mas optima para moverse */
+		/* pre: la lista de posiciones debe tener al menos una posicion a ir */
+		/* post: cambia la posicion actual a la posicion siguiente, que es mas eficiente */
 		Posicion nuevaPosicion = new Posicion ();
 		
 		return nuevaPosicion;
 		
+	}
+	
+	public Posicion getPrimerPosicion(){
+		/* Devuelve la primer posicion destino de la trayectoria */
+		/* post: devuelve la primer posicion, o null de no haber ningun destino definido */
+		return this.listaDePosiciones.get(0);
 	}
 		
 }
