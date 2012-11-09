ce0e58c9c046f94a14915ae7be3159ec81f6328b
gvallerino <gvallerino@c7857a9e-41aa-a20c-209e-0a810bdc360d>
2012-11-09 20:14:30 +0000

    Agregué tests de la trayectoria. Andan todos ahora.

--- trunk/test/fiuba/algo3/flightcontrol/TrayectoriaTest.java
+++ trunk/test/fiuba/algo3/flightcontrol/TrayectoriaTest.java
@@ -4,35 +4,110 @@ import java.util.ArrayList;
 
 public class TrayectoriaTest extends TestCase {
 	
+	private Posicion actual,destino,proximoPaso;
+	private ArrayList<Posicion> puntos;
+	private Trayectoria trayectoria;
+	
+	protected void setUp () throws Exception {
+		super.setUp();
+		
+		actual = new Posicion (1, 1);
+		puntos = new ArrayList<Posicion>();
+	}
 	
 	public void testProximaPosicionDeberiaRetornarUnaPosicionParaContinuarConlaDireccionQueTraia(){
 		
-   //arrange
-		int x1 = 1;
-		int y1 = 1;
-		int x2 = 1;
-		int y2 = 2;
+		//arrange	
+		destino = new Posicion (4, 4);
+		proximoPaso = new Posicion (2,2);
+		Posicion proximaPosicion;
 		
-		Posicion posicion1 = new Posicion (x1, y1);
-		Posicion posicion2 = new Posicion (x2, y2);
-
+		puntos.add(destino);		
 		
-		ArrayList<Posicion> puntos = new ArrayList<Posicion>();
-		puntos.add(posicion1);
-		puntos.add(posicion2);
+		trayectoria = new Trayectoria (puntos);
+		//act
 		
-		boolean direccionesIguales;
-		
-		
-	//act
-		Trayectoria trayectoria = new Trayectoria (puntos);
-		Posicion proximaPosicion = trayectoria.calcularProximaPosicion(posicion1);
-		
-		direccionesIguales = proximaPosicion.equals(posicion2);
-		
-	//assert
-		assertTrue (direccionesIguales);
+		proximaPosicion = trayectoria.getProximaPosicion(actual);
+				
+		//assert
+		assertTrue (proximaPosicion.equals(proximoPaso));
+	}
 	
+	public void testObtenerVectorDirectorDeberiaDarUnVectorQueIndiqueLaDireccionASeguirParaLlegarADestino (){
+		
+		//arrange
+		destino = new Posicion (4, 4);
+		Posicion direccion = new Posicion (1,1);
+		
+		puntos.add(destino);
+		
+		trayectoria = new Trayectoria (puntos);
+		
+		//act
+		Posicion direccionDeLaTrayectoria = trayectoria.getVectorDirector(actual);
+				
+		//assert
+		assertTrue (direccionDeLaTrayectoria.equals(direccion));
+	}
+	
+	public void testRecorrerLaTrayectoriaDeberiaObtenerTodosLosPuntosCorrectamente (){
+		
+		//arrange
+		destino = new Posicion (7,5);
+		proximoPaso = new Posicion (1,1);
+		Posicion direccion;
+		boolean pasoCorrecto = true;
+		
+		puntos.add(destino);
+		trayectoria = new Trayectoria (puntos);
+		
+		//act
+		
+		//Primero se mueve en linea recta
+		
+		direccion = new Posicion (1,0);
+		while (pasoCorrecto){
+			
+			proximoPaso = trayectoria.getProximaPosicion(actual);
+			actual = actual.sumar(direccion);
+			pasoCorrecto = actual.equals(proximoPaso);
+			
+		}
+		
+		//Luego se mueve diagonalmente
+		
+		direccion = new Posicion (1,1);
+		actual = proximoPaso;
+		pasoCorrecto = actual.equals(proximoPaso);
+		
+		while (pasoCorrecto && ! proximoPaso.equals(destino)){
+			
+			proximoPaso = trayectoria.getProximaPosicion(actual);
+			
+			actual = actual.sumar(direccion);
+			
+			pasoCorrecto = actual.equals(proximoPaso);
+			
+			
+		}
+		
+		//assert
+		assertTrue (pasoCorrecto);
+	}
+	
+	public void testDespuesDeObtenerLaUltimaPosicionDeLaTrayectoriaDeberiaDevolverNull (){
+		
+		//arrange
+		destino = new Posicion (2,2);
+		puntos.add(destino);
+		trayectoria = new Trayectoria (puntos);
+		
+		//act
+		proximoPaso = trayectoria.getProximaPosicion(actual);
+		proximoPaso = trayectoria.getProximaPosicion(actual);
+		
+		//assert
+		assertTrue (proximoPaso == null);
 		
 	}
 
