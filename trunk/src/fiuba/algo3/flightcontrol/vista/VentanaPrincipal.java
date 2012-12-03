659369d21dad9a7a0a9ba7fa66cfe0243cf888a4
joelruggieri@gmail.com <joelruggieri@gmail.com@c7857a9e-41aa-a20c-209e-0a810bdc360d>
2012-12-03 14:20:31 +0000

    se hicieron las vistas de pistas, y se intento hacer la parte para setear la trayectoria.

--- trunk/src/fiuba/algo3/flightcontrol/vista/VentanaPrincipal.java
+++ trunk/src/fiuba/algo3/flightcontrol/vista/VentanaPrincipal.java
@@ -1,7 +1,9 @@
 package fiuba.algo3.flightcontrol.vista;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -9,14 +11,19 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import fiuba.algo3.flightcontrol.modelo.Nivel;
+import fiuba.algo3.flightcontrol.modelo.ObjetoVolador;
 import fiuba.algo3.flightcontrol.modelo.Pista;
+import fiuba.algo3.flightcontrol.modelo.Trayectoria;
+import fiuba.algo3.flightcontrol.modelo.Vector;
 
 import fiuba.algo3.titiritero.dibujables.Cuadrado;
 import fiuba.algo3.titiritero.dibujables.SuperficiePanel;
@@ -27,6 +34,9 @@ public class VentanaPrincipal {
 
 	private JFrame frame;
 	private GameLoop gameLoop;
+	private Dimension tamano;
+	
+
 
 	/**
 	 * Launch the application.
@@ -49,6 +59,10 @@ public class VentanaPrincipal {
 	 */
 	public VentanaPrincipal() {
 		try {
+			
+			Toolkit tk = Toolkit.getDefaultToolkit();
+			tamano = tk.getScreenSize();
+			
 			initialize();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -61,49 +75,120 @@ public class VentanaPrincipal {
 	 * @throws IOException 
 	 */
 	private void initialize() throws IOException {
+				
 		frame = new JFrame();
 		frame.setForeground(new Color(0, 0, 0));
-		frame.setBounds(700, 300, 470, 470);
+		frame.setBounds(0, 0, tamano.width, tamano.height);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		frame.setTitle("Flight Control by Key");
+		frame.setTitle("Flight Control");
 		
 		JButton btnIniciar = this.addBotonIniciar();
 		
 		JButton btnDetener = this.addBotonDetener();
 		
+		JButton btnSetTrayectoria = this.addBotonSetTrayectoria();
+		
 		JPanel panel = this.addSuperficiePanel();
 		
 		this.gameLoop = new GameLoop((SuperficieDeDibujo) panel);
 		
-		this.inicializarModelo((SuperficieDeDibujo) panel);
+		final Nivel unNivel = new Nivel(10,tamano.height-60);
+		final List<Vector> unaLista = new ArrayList<Vector>(); 
 		
-		this.addMouseListener(panel);
-		
+		this.inicializarModelo((SuperficieDeDibujo) panel, unNivel);
+				
 		this.addKeyListener();
 
-		this.setComponentsFocus(btnIniciar, btnDetener);
+		this.setComponentsFocus(btnIniciar, btnDetener, btnSetTrayectoria);
 
-	}
+		
+				
+		panel.addMouseListener(new MouseAdapter() {
+				
+			private ObjetoVolador unAvion;
+			private List<Vector> unaLista = new ArrayList<Vector>();
+			
+			@Override
+			public void mouseClicked(MouseEvent click) {	
+				
+				//System.out.println("x = " + arg0.getX());
+				//System.out.println("y = " + arg0.getY());
+				//unaLista.add(new Vector(100, 100));
+				
+				if (click.isAltDown()) {
+					if (this.unAvion == null) {
+					
+						this.unAvion = this.obtenerAvion(click);
+						System.out.println("AAAAAAAAAAAAAAAAAAAAAAAA");
+					
+					} else {
+						
+						Vector posicionClick = new Vector (click.getX(),click.getY());
+						unaLista.add(posicionClick);
+						System.out.println("BBBBBBBBBBBBBBBBBBBBBB");
+						
+					}
+					
+				} else {
+						
+					if (this.unAvion != null) {
+						Trayectoria unaTrayectoria = new Trayectoria(unaLista);
+						this.unAvion.setTrayectoria(unaTrayectoria);
+						this.unAvion = null;
+						this.unaLista.clear();
+						System.out.println("CCCCCCCCCCCCCCCCCCCCCCC");
+					}
+						
+				}
+					
+			}
+		
 
-	private void inicializarModelo(SuperficieDeDibujo unPanel) {
-		Nivel unNivel = new Nivel(10,350);
+			private ObjetoVolador obtenerAvion(MouseEvent click) {
+				boolean encontrado = false;
+				ObjetoVolador unAvion, avionEncontrado = null;
+				Vector posicionClick = new Vector (click.getX(),click.getY());
+				Vector posicionDeAvion;
+				double diferencia;
+				
+				Iterator<ObjetoVolador> it = unNivel.getObjetosVoladores();
+				while (it.hasNext() && !encontrado) {
+					unAvion = it.next();
+					posicionDeAvion = unAvion.getPosicion();
+					diferencia =  posicionClick.distancia(posicionDeAvion);
+					encontrado = (diferencia <= 20);
+					
+					if (encontrado){ 
+						avionEncontrado = unAvion;
+					}
+				}
+				
+				return avionEncontrado;
+			}});
+		}
+
+	private void inicializarModelo(SuperficieDeDibujo unPanel, Nivel unNivel) {
+		
 		ObservadorDeNivel observadorDeNivel = new ObservadorDeNivel(gameLoop, (SuperficieDeDibujo)unPanel, unNivel);
 		unNivel.addObserver(observadorDeNivel);
+		ObservadorDeGameLoop observadorGameLoop = new ObservadorDeGameLoop(observadorDeNivel);
+		//this.gameLoop.agregarObservador(observadorGameLoop);
 		this.gameLoop.agregar(unNivel);
 	
 		Iterator<Pista> it = unNivel.getPistas();
 		while (it.hasNext()) {
 			Pista unaPista = it.next();
-			Cuadrado cuadrado = new VistaPista(unaPista);
+			Rectangulo cuadrado = new VistaPista(unaPista);
 			this.gameLoop.agregar(cuadrado);
 		}		
 	}
 
-	private void setComponentsFocus(JButton btnIniciar, JButton btnDetener) {
+	private void setComponentsFocus(JButton btnIniciar, JButton btnDetener, JButton btnSetTrayectoria) {
 		frame.setFocusable(true);
 		btnDetener.setFocusable(false);
 		btnIniciar.setFocusable(false);
+		btnSetTrayectoria.setFocusable(false);
 		
 	}
 
@@ -113,7 +198,8 @@ public class VentanaPrincipal {
 			
 			@Override
 			public void keyTyped(KeyEvent arg0) {
-				System.out.println("Key pressed");
+				//System.out.println("Key pressed");
+
 			}
 			
 			@Override
@@ -122,30 +208,32 @@ public class VentanaPrincipal {
 			
 			@Override
 			public void keyPressed(KeyEvent arg0) {
-				System.out.println("Ping");
+				//System.out.println("Ping");
 			}  
 			 	
 		});
 	}
-
-	private void addMouseListener(JPanel panel) {
-		
-		panel.addMouseListener(new MouseAdapter() {
-					
-			@Override
-			public void mouseClicked(MouseEvent arg0) {	
-
-				//unaLista.add(new Vector(100, 100));
-
-			}});
-	}
-
+	
 	private JPanel addSuperficiePanel() {
 		JPanel panel = new SuperficiePanel();
-		panel.setBackground(new Color(0, 0, 0));
-		panel.setBounds(42, 53, 370, 370);
+		panel.setBackground(new Color(0, 150, 0));
+		System.out.println(tamano.width);
+		System.out.println(tamano.height);
+		panel.setBounds(350, 5, tamano.height-40, tamano.height-40);
 		frame.getContentPane().add(panel);
 		return panel;
+	}
+	
+	private JButton addBotonIniciar() {
+		JButton btnIniciar = new JButton("Iniciar");
+		btnIniciar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				gameLoop.iniciarEjecucion();
+			}
+		});
+		btnIniciar.setBounds(50, 20, 77, 25);
+		frame.getContentPane().add(btnIniciar);
+		return btnIniciar;
 	}
 
 	private JButton addBotonDetener() {
@@ -155,20 +243,15 @@ public class VentanaPrincipal {
 				gameLoop.detenerEjecucion();
 			}
 		});
-		btnDetener.setBounds(325, 16, 92, 25);
+		btnDetener.setBounds(50, 50, 92, 25);
 		frame.getContentPane().add(btnDetener);
 		return btnDetener;
 	}
-
-	private JButton addBotonIniciar() {
-		JButton btnIniciar = new JButton("Iniciar");
-		btnIniciar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				gameLoop.iniciarEjecucion();
-			}
-		});
-		btnIniciar.setBounds(42, 16, 77, 25);
-		frame.getContentPane().add(btnIniciar);
-		return btnIniciar;
+	
+	private JButton addBotonSetTrayectoria() {
+		JButton btnSetTrayectoria = new JButton("Set Trayectoria");
+		btnSetTrayectoria.setBounds(50, 110, 130, 25);
+		frame.getContentPane().add(btnSetTrayectoria);
+		return btnSetTrayectoria;
 	}
 }
